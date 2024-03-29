52226f6f9c85fbd70ef3e1eb9dfe87c56a52c23c
Nikki <niharikagobboor@gmail.com>
2024-03-29 13:07:58 +0530

    Library mysql code commit to remote main branch

---
+++ app.py
@@ -0,0 +1,88 @@
+from flask import Flask, render_template, request, jsonify
+import mysql.connector
+
+app = Flask(__name__)
+
+# def handle_error(message, status_code=400): 
+#     return jsonify({"error": message}), status_code
+
+DATABASE_CONFIG = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': 'Abhishek@123',
+    'database': 'nikki_db'
+}
+
+def fetch_data():   
+    
+    try:
+        connection = mysql.connector.connect(**DATABASE_CONFIG)
+        cursor = connection.cursor(dictionary=True)  
+    except mysql.connector.Error as err:
+        return jsonify({'error': f'Database connection error: {err}'}), 500
+    try:
+        cursor.execute("SELECT * FROM books")
+        data = cursor.fetchall()
+        print(data)
+        book_data = []
+        for document in data: 
+            print (document)
+            document['_id'] = str(document['_id'])
+            book_data.append(document)
+    except mysql.connector.Error as err:
+        return jsonify({'error': f'Query execution error: {err}'}), 500
+
+    finally:
+        if connection.is_connected():
+            connection.close()
+            
+
+
+    # Convert data to JSON
+    # json_data = jsonify(data)
+
+    # return json_data
+    print(book_data)
+    return book_data
+
+@app.route('/data')
+def display_data():
+    tempdata=fetch_data()     
+    json_all_data = jsonify({'my_books': tempdata})
+    print(json_all_data)
+    return json_all_data
+
+# Route for index page
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/books", methods=["GET"])
+def search_books():
+    getbook = request.get_json()
+    print(getbook)
+    if not getbook:
+        return jsonify ({'error': 'No data provided'}), 400
+    book_id = getbook.get('Book_id')
+    print(book_id)
+    title = getbook.get('title')
+    print(title)
+    author = getbook.get('Author') # Validate data
+    print(author)
+    sql = "INSERT INTO books (book_id, Title, Author) VALUES (%s, %s, %s)"
+    val = (book_id, title, author)   
+    cursor.execute(sql, val)
+    if not title or not author:
+        return jsonify({'error': 'Missing required fields'}), 400
+# Process data (store in database, etc. ) #
+    return jsonify({'message': 'Book added success fully '}), 201
+
+# Commit the changes
+connection.commit()
+
+# Close the connection
+connection.close()
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
